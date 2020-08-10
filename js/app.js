b5e7d2ce1fc3c8eb507985aa4495d62d7d549b82
oshadiw <oshadiw@gmail.com>
2020-08-10 16:12:04 -0500

    message

--- js/app.js
+++ js/app.js
@@ -23,3 +23,22 @@ data.forEach((dataRow) => {
         }   
     );
 });
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if a date was entered and filter the data using that date
+    if (date) {
+        // apply filter to the table data to only keep the rows where the datetime value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    // rebuild the table using the filtered data
+    buildTable(filteredData);
+}
+
+// attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
+
